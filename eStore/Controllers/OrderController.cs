ebf7316cdfb5039410ea90211afabba103f1bec4
QuocLoc12 <nguyenloc120121@gmail.com>
2022-07-23 21:24:29 +0700

    add Order management

---
+++ eStore/Controllers/OrderController.cs
@@ -0,0 +1,132 @@
+﻿using DataAccess.Models;
+using DataAccess.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace eStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        IOrderRepository orderRepository = null;
+        public OrderController() => orderRepository = new OrderRepository();
+        // GET: OrderController
+        public ActionResult OrderPage()
+        {
+            var orderList = orderRepository.GetOrders();
+            return View(orderList);
+        }
+
+        // GET: OrderController/Details/5
+        public ActionResult OrderDetails(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var order = orderRepository.GetOrder(id.Value);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return View(order);
+        }
+
+        // GET: OrderController/Create
+        public ActionResult CreateOrder()
+        {
+            return View();
+        }
+
+        // POST: OrderController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateOrder(Order order)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    orderRepository.InsertOrder(order);
+                }
+                return RedirectToAction(nameof(OrderPage));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = ex.Message;
+                return View(order);
+            }
+        }
+
+        // GET: OrderController/Edit/5
+        public ActionResult EditOrder(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var order = orderRepository.GetOrder(id.Value);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return View(order);
+        }
+
+        // POST: OrderController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditOrder(int id, Order order)
+        {
+            try
+            {
+                if (id != order.OrderId)
+                {
+                    return NotFound();
+                }
+                if (ModelState.IsValid)
+                {
+                    orderRepository.UpdateOrder(order);
+                }
+                return RedirectToAction(nameof(OrderPage));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = ex.Message;
+                return View();
+            }
+        }
+
+        // GET: OrderController/Delete/5
+        public ActionResult DeleteOrder(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var order = orderRepository.GetOrder(id.Value);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return View(order);
+        }
+
+        // POST: OrderController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteOrder(int id, IFormCollection collection)
+        {
+            try
+            {
+                orderRepository.DeleteOrder(id);
+                return RedirectToAction(nameof(OrderPage));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = ex.Message;
+                return View();
+            }
+        }
+    }
+}
